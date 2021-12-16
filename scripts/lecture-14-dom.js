132b395464711c7d097ff3258bfa2145977d69a3
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-12-16 17:52:10 +0200

    Lecture 14 add custom color to element

--- scripts/lecture-14-dom.js
+++ scripts/lecture-14-dom.js
@@ -9,11 +9,17 @@ console.log("Will manipulate DOM elements for food!");
 const resultParent = document.getElementById("results");
 
 const genButton = document.getElementById("gen-btn");
+const clearButton = document.getElementById("clear-all-btn");
+
+
 const elCount = document.getElementById("el-count");
+const colorPicker = document.getElementById("color-inp");
+
+
 let idCount = 0
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-function createNewElement(parent, tag, id="", classes=[], text) {
+function createNewElement(parent, tag, id="", classes=[], text="", color="") {
     const newEl = document.createElement(tag);
       // and give it some content
     const newContent = document.createTextNode(text);
@@ -30,12 +36,20 @@ function createNewElement(parent, tag, id="", classes=[], text) {
         newEl.classList.add(newClass);
     }
 
+    //if there is any color passed we will change background color
+    if (color) {
+        newEl.style.backgroundColor = color;
+    }
+
 
     //before the next command, the new element exists but is not attached to the DOM tree
     parent.appendChild(newEl);
 }
 
 function onGenerateElementsClick(ev) {
+    //we will first clear the old elements if we have any
+    onClearAllElements();
+
     const count = parseInt(elCount.value);
     console.log("Will generate elements. How many? - " + count);
 
@@ -43,15 +57,20 @@ function onGenerateElementsClick(ev) {
     for (let i=0; i<count; i++) {
         const txt = "Element No." + i;
 
-        classArray = ["strong-el"];
+        let bgColor = "";
+        let classArray = ["strong-el"];
+
         //now depending on the reminder (odd /even here)
         //we will add another class to our class names array
         if (i % 2 == 0) {
             classArray.push("dark-col");
+            
+            //so for even index numbers we grab the color value from our color picker
+            bgColor = colorPicker.value; 
         } else { //odd reminder is 1
             classArray.push("light-col");
         }
-        createNewElement(resultParent, "div", id=i, classes=classArray, text=txt);
+        createNewElement(resultParent, "div", id=i, classes=classArray, text=txt, color=bgColor);
     }
     
 }
@@ -59,3 +78,19 @@ function onGenerateElementsClick(ev) {
 //alternative would be 
 //addEventListener('click', onGenerateElementsClick)
 genButton.onclick = onGenerateElementsClick //notice no parenthesis!
+
+function onClearAllElements() {
+    console.log("Will clear all elements");
+    //TODO actually clear ALL of them!
+    //we could remove from both ends one by one - you could have a button for this!
+    // resultParent.removeChild(resultParent.firstChild); //so this removes just the first child
+    // resultParent.removeChild(resultParent.lastChild); //so this removes just the last child
+    //for all removing ALl elements we will use a while loop
+    //https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
+    while (resultParent.firstChild) {
+        resultParent.removeChild(resultParent.firstChild);
+      }
+    
+}
+
+clearButton.onclick = onClearAllElements;
