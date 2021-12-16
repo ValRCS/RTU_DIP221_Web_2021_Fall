ed470bc2657b5a094732a7c9ce79737446a22571
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-12-16 17:15:56 +0200

    Create multiple dom elements with loop

--- scripts/lecture-14-dom.js
+++ scripts/lecture-14-dom.js
@@ -10,15 +10,28 @@ const resultParent = document.getElementById("results");
 
 const genButton = document.getElementById("gen-btn");
 const elCount = document.getElementById("el-count");
+let idCount = 0
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-function createNewElement(parent, tag, id="", classes="", text) {
+function createNewElement(parent, tag, id="", classes=[], text) {
     const newEl = document.createElement(tag);
       // and give it some content
     const newContent = document.createTextNode(text);
     // add the text node to the newly created div
     newEl.appendChild(newContent);
+    newEl.id = "res-el-" + id; //so we add/modify id attribute for this element
+    //we could use a global variable if we are worried about overusing the same id
+    // newEl.id = "res-el-" + (id+idCount); //so we add/modify id attribute for this element
+    // idCount++; //this is a global variable
+    //we could have many classes so we are going to go through this array of classes
+    //and add them one by one, 
+    //default argument is array of 0 elements so this loop will not run
+    for (const newClass of classes) {
+        newEl.classList.add(newClass);
+    }
 
+
+    //before the next command, the new element exists but is not attached to the DOM tree
     parent.appendChild(newEl);
 }
 
@@ -26,7 +39,11 @@ function onGenerateElementsClick(ev) {
     const count = parseInt(elCount.value);
     console.log("Will generate elements. How many? - " + count);
 
-    createNewElement(resultParent, "div", id=3, classes="", text="Hello there!");
+    //we will create a loop to create as many elements as our count indicates
+    for (let i=0; i<count; i++) {
+        createNewElement(resultParent, "div", id=i, classes=["strong-el", "dark-col"], text="Hello there!");
+    }
+    
 }
 
 //alternative would be 
